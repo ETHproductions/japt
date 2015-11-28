cbc719eadc69335860afd73b0c69970e583f0e1f
ETHproductions <eth3792@nycap.rr.com>
2015-11-28 16:09:58 -0500

    Bug fix: char after uppercase letter is ignored

--- src/japt-interpreter.js
+++ src/japt-interpreter.js
@@ -475,6 +475,7 @@ function transpile(code) {
             }
             else {
                 outp += letters.split("").join(",");
+                outp += code[i];
             }
         }
         else if (char === "'") {

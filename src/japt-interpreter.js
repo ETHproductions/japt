d5b58110724da97beaa6133d9a96796828d66825
ETHproductions <eth3792@nycap.rr.com>
2016-01-23 23:09:41 -0500

    debugging

--- src/japt-interpreter.js
+++ src/japt-interpreter.js
@@ -10,7 +10,7 @@ function workerify(){
 	if (window.Worker) {
 		var s = document.createElement("script");
 		s.type = "text/js-worker";
-		s.innerHTML = 'alert("hey!");var data = e.data, value, success = !0;alert("data: "+data);if(data[0]==="transpile") value = transpile(data[1]);else {try {value = eval(data[1]);} catch(e) {success = !1; value = e;}}postMessage([success,value]);close();'
+		s.innerHTML = 'window.alert("hey!");var data = e.data, value, success = !0;window.alert("data: "+data);if(data[0]==="transpile") value = transpile(data[1]);else {try {value = eval(data[1]);} catch(e) {success = !1; value = e;}}postMessage([success,value]);close();'
 		document.body.appendChild(s);
 
 		var blob = new Blob(Array.prototype.map.call(document.querySelectorAll("script[type=\"text\/js-worker\"]"), function (oScript) {
